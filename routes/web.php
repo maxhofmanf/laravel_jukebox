f791aab5b44a96334200120193658057fbf42294
maxhofmanf <99068281@mydavinci.nl>
2023-06-26 13:32:22 +0200

    added routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 use App\Http\Controllers\GenreController;
+use App\Http\Controllers\SongController;
+use App\Http\Controllers\PlaylistController;
+
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -35,4 +38,16 @@ Route::get('/genre/all', [GenreController::class, 'index'])->name('genre.index')
 Route::get('/genre/create', [GenreController::class, 'create'])->name('genre.create');
 Route::post('/genre/store',[GenreController::class, 'store'])->name('genre.store');
 Route::get('/genre/destroy/{genre}', [GenreController::class, 'destroy'])->name('genre.destroy');
+
+Route::get('/song/all', [SongController::class, 'index'])->name('song.index');
+Route::get('/song/create', [SongController::class, 'create'])->name('song.create');
+Route::post('/song/store',[SongController::class, 'store'])->name('song.store');
+Route::get('/song/destroy/{song}', [SongController::class, 'destroy'])->name('song.destroy');
+
+
+Route::get('/playlist/all', [PlaylistController::class, 'index'])->name('playlist.index');
+Route::get('/playlist/create', [PlaylistController::class, 'create'])->name('playlist.create');
+Route::post('/playlist/store',[PlaylistController::class, 'store'])->name('playlist.store');
+Route::get('/playlist/destroy/{playlist}', [PlaylistController::class, 'destroy'])->name('playlist.destroy');
+
 
